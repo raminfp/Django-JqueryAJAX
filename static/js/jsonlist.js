63cbcb40683a60ee73aab482a39effcb6b0eb0d8
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-21 09:32:07 +0330

    Update jsonlist.js

--- static/js/jsonlist.js
+++ static/js/jsonlist.js
@@ -1,1 +1,30 @@
 
+var jsonitem = []
+  
+item = {};
+item ["one"] = $(".one").val();
+item ["two"] = $(".two").val();
+item ["three"] = $(".three").val();
+item ["four"] = $(".four").val();
+jsonitem.push(item);
+
+// Send with JQuery - AJAX
+
+ model = jsonitem;
+    $.ajax({
+        url: "/api/contact",
+        datatype: 'json',
+        type: 'POST',
+        data: {'model[]': JSON.stringify(model)},
+        success: function (data) {
+            $(".seccuess").html(data).show();
+            $("#firstname").val("");
+            $("#lastname").val("");
+            $("#connect").val("");
+            $("#decreption").val("");
+        },
+        
+// Get List of Django on view.py
+
+listmode = request.POST.getlist('model[]')
+  
