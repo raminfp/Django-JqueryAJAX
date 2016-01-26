dbae1a78a32569036765ab6fe03c541a1eb140a0
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-26 08:54:54 +0330

    Create UploadImage.py
    Get POST AJAX image from JQuery Convert to binary for store DB

---
+++ Controller/Ajax/UploadImage.py
@@ -0,0 +1,20 @@
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+import os
+from Tutorial_Samin import settings
+
+
+def UploadIMG(request):
+
+    if request.POST:
+        Getimg = request.FILES.dict()
+        image = Getimg['file']
+        f = ContentFile(image.read())
+        path = default_storage.save('C:\\Users\\RaminFP\\Desktop\\tmp\\1.png', f)
+        tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+        img = open(tmp_file.__str__(),'rb').read()
+        o = Student(image=img)
+        InsertDB(o)
+        os.remove(path)
+
+    return render(request,"UploadImage.html")
