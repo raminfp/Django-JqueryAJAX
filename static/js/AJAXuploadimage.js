8f83ba9ffa3ee6d3e607b6ed8a8d79573086e0fa
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-26 08:50:57 +0330

    Create AJAXuploadimage.js
    Uplaod Image with JQuery AJAX

---
+++ static/js/AJAXuploadimage.js
@@ -0,0 +1,45 @@
+$(document).ready(function (e) {
+
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    var csrftoken = getCookie('csrftoken');
+
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    });
+
+
+
+    $("#uploadimage").on('submit', (function (e) {
+        e.preventDefault();
+        alert(1);
+        $.ajax({
+            url: "/select", // Url to which the request is send
+            type: "POST",             // Type of request to be send, called as method
+            data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+            contentType: false,       // The content type used when sending data to the server.
+            cache: false,             // To unable request pages to be cached
+            processData: false,        // To send DOMDocument or non processed data file it is set to false
+            success: function (data)   // A function to be called if request succeeds
+            {
+                $('#loading').hide();
+                $("#message").html(data);
+            }
+        });
+    }));
+});
