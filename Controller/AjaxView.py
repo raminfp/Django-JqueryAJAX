91a1d126badef0400edc974b10d25934b2c6dfe7
Ramin Farajpour Cami <ramin.blackhat@gmail.com>
2016-01-26 08:52:03 +0330

    Update AjaxView.py

--- Controller/AjaxView.py
+++ Controller/AjaxView.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def uploadimage(request):
+    
+    return render(request, 'uploadimage.html')
+    
